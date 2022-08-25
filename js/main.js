b44ed17f1ab3e88e8ff2788128c8e27f8989fe97
javad5383 <javad.mohammadi5383@gmail.com>
2022-08-25 13:23:37 +0430

    update1

--- js/main.js
+++ js/main.js
@@ -54,3 +54,124 @@ function openMenu() {
         $("#res-list").attr("data-open","close");
       }
     }
+
+
+
+
+    /*start light sliders*/
+
+    $(document).ready(function () {
+        $("#lightSlider").lightSlider({
+            rtl: true,
+            item: 5,
+            autoWidth: true,
+            slideMove:1,
+            loop:true,
+            pager: false,
+            speed:200, //ms'
+            auto: false,
+            pause:8000,
+            enableTouch:true,
+            enableDrag:true,
+            freeMove:false,
+            swipeThreshold: 40,
+
+        });
+    });
+
+     /*End light sliders*/
+
+     //Main Slider//
+
+     $(document).ready(function () {
+        $("#lightSlider3").lightSlider({
+            rtl: true,
+            item: 3,
+            autoWidth: true,
+            loop: true,
+            pager: false,
+            speed: 2000, //ms'
+            auto: true,
+            pause: 5000,
+
+
+            slideEndAnimation: true,
+            responsive : [
+                {
+                    breakpoint:768,
+                    settings:{
+                        item:1,
+                        slideMargin:16
+                    }
+
+
+                    
+                }
+               
+
+            ],
+
+        });
+    });
+
+     // End Main Slider//
+
+     $(document).ready(function () {
+        $("#lightSlider2").lightSlider({
+            rtl: true,
+            item: 5,
+            autoWidth: true,
+
+            pager: false
+        });
+    });
+
+
+    $(document).ready(function () {
+        $("#lightSlider4").lightSlider({
+            rtl: true,
+            item: 5,
+            autoWidth: true,
+
+            pager: false
+        });
+    });
+
+    $(document).ready(function () {
+        $("#lightSlider5").lightSlider({
+            rtl: true,
+            item: 5,
+            autoWidth: true,
+
+            pager: false
+        });
+    });
+
+    $(document).ready(function () {
+        $("#lightSlider6").lightSlider({
+            rtl: true,
+            item: 5,
+            autoWidth: true,
+            loop: true,
+            pager: false,
+            speed: 2000, //ms'
+            auto: true,
+            pause: 5000,
+
+        });
+    });
+
+
+
+
+
+
+    if (window.innerWidth > 1024 ) {
+        console.log("fgfcvgb")
+        $("#move-this").appendTo("#move-here");
+        
+    }
+
+
+
+   
